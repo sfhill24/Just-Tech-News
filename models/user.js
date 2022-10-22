02c3a2e5159c8156c33abe329a91b34d8f41b65c
Shelly Foxworth-Hill <sfhill24@gmail.com>
2022-10-22 19:05:10 -0400

    deployed to heroku

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model { }
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
+// create fields/columns for User model
 User.init(
   {
     id: {
@@ -36,17 +42,15 @@ User.init(
   {
     hooks: {
       // set up beforeCreate lifecycle "hook" functionality
-      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
-
     },
     sequelize,
     timestamps: false,
@@ -55,4 +59,5 @@ User.init(
     modelName: 'user'
   }
 );
+
 module.exports = User;
